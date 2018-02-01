f98a8598945f5b34c37bb4f2ff8ce447aca5ca30
Richard Morgan <mba.rick@gmail.com>
2018-01-31 21:22:31 -0600

    Initial commit

---
+++ HitCounter/HitCounterHub.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Hubs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace HitCounter
+{
+	[HubName("HitCounter")]
+	public class HitCounterHub : Hub
+	{
+		static int _hitCount = 0;
+
+		public override Task OnConnected()
+		{
+			_hitCount += 1;
+			base.Clients.All.hitReceived(_hitCount);
+			return base.OnConnected();
+		}
+
+		public override Task OnDisconnected(bool stopCalled)
+		{
+			_hitCount -= 1;
+			base.Clients.All.hitReceived(_hitCount);
+			return base.OnDisconnected(stopCalled);
+		}
+	}
+}
